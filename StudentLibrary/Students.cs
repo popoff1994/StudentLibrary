681a08f5c215d5d9ba7b34a3275eeac195a42b45
popoff1994 <jonasmadsenfake@gmail.com>
2022-11-29 10:15:05 +0100

    Add project files.

---
+++ StudentLibrary/Students.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentLibrary
+{
+    public class Students
+    {
+        public int StudentId { get; set; }
+        [MinLength(2, ErrorMessage ="Name must be atleast two characters long")]
+        public string Name { get; set; }
+        public int AbsenceMin { get; set; }
+        public int NFCId { get; set; }
+
+        public Students(int studentId, string name, int absenceMin, int nFCId)
+        {
+            StudentId = studentId;
+            Name = name;
+            AbsenceMin = absenceMin;
+            NFCId = nFCId;
+        }
+        public Students()
+        {
+
+        }
+        public void NameValidatior()
+        {
+            if (Name.Length < 2)
+                throw new ArgumentException("NAME MUST BE AT LEAST TWO CHARACTERS LONG");
+            else
+                Console.WriteLine("Name Validated");
+        }
+    }
+}
