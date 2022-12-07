fb67c43c49537b6dc5efa240c1c2ebd3aedbc496
Jonas Madsen <82613909+popoff1994@users.noreply.github.com>
2022-12-07 10:03:00 +0100

    Update Students.cs

--- StudentLibrary/Students.cs
+++ StudentLibrary/Students.cs
@@ -8,14 +8,16 @@ namespace StudentLibrary
         [MinLength(2, ErrorMessage ="Name must be atleast two characters long")]
         public string Name { get; set; }
         public int AbsenceMin { get; set; }
+        public bool CheckedIn { get; set; }
         public int NFCId { get; set; }
 
-        public Students(int studentId, string name, int absenceMin, int nFCId)
+        public Students(int studentId, string name, int absenceMin, int nFCId, bool checkedIn)
         {
             StudentId = studentId;
             Name = name;
             AbsenceMin = absenceMin;
             NFCId = nFCId;
+            CheckedIn = checkedIn
         }
         public Students()
         {
